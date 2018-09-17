62544d2fee3a7f8f2fb9117a3443f183bb665e77
Alastair Edmonds <alastair.edmonds@gmail.com>
2018-09-17 16:29:28 +0100

    rename

--- note.js
+++ note.js
@@ -1,20 +1,12 @@
-// (function(exports) {
-//   function Note() {
-//     console.log('hello')
-//   }
-
-//   exports.Note = Note 
-// })(this) 
-
 (function (exports) {
-  function NoteModel(string) {
+  function Note(string) {
     this.text = string;
   };
 
-  NoteModel.prototype.textReturn = function () {
+  Note.prototype.textReturn = function () {
     return this.text;
   };
 
-  exports.NoteModel = NoteModel;
+  exports.Note = Note;
 
 })(this);
--- spec/units/note-tests.js
+++ spec/units/note-tests.js
@@ -1,20 +1,8 @@
-// import { Note } from './note.js'
-
-// (function(exports) {
-//   function noteTest1() {
-//     var note = new Note()
-//     if (!note) {
-//     throw new Error('no note thing exists, this means Note is undefined')
-//     }
-//   }
-//   noteTest1()
-//   })(this)
-
 (function (exports) {
 
   function testNoteInstantiation() {
-    var noteModel = new NoteModel("Test note");
-    assert.isTrue(noteModel.text === "Test note")
+    var note = new Note("Test note");
+    assert.isTrue(note.text === "Test note")
   };
 
   testNoteInstantiation();
