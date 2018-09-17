2100ea86b52546a833b9a669f34c0633aab8d606
josh nickson <jnickson1506@gmail.com>
2018-09-17 15:39:34 +0100

    change file structure to house testing files

--- spec/units/note-tests.js
+++ spec/units/note-tests.js
@@ -14,11 +14,7 @@
 
   function testNoteInstantiation() {
     var noteModel = new NoteModel("Test note");
-
-    if (noteModel.textReturn() !== "Test note") {
-      throw new Error("Note not instantiated correctly")
-    } else { console.log("testNoteInstantiation passed") }
-
+    assert.isTrue(noteModel.text === "Test note")
   };
 
   testNoteInstantiation();
